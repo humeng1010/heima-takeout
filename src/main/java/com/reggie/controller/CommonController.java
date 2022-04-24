4292839676a918cd6f0419e780409d829bd9bc2d
小胡 <1589029587@qq.com>
2022-04-24 10:34:15 +0800

    Day04-2

--- src/main/java/com/reggie/controller/CommonController.java
+++ src/main/java/com/reggie/controller/CommonController.java
@@ -3,12 +3,17 @@ package com.reggie.controller;
 import com.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -39,6 +44,14 @@ public class CommonController {
 
         //使用UUID重新生成文件名，防止文件名重复造成文件覆盖
         String fileName = UUID.randomUUID() +suffix;//21da3k41b4.jpg 我们要获取原始文件的后缀，进行组合
+
+        //创建目录
+        File dir = new File(basePath);
+        //判断目录是否存在
+        if (!dir.exists()){
+            //如果目录不存在需要创建
+            dir.mkdirs();
+        }
         try {
             //将临时文件转存到指定位置
             //自定义文件名
@@ -46,10 +59,43 @@ public class CommonController {
             //使用原始文件名
 //            file.transferTo(new File(basePath+originalFilename));
             //使用UUID
-            file.transferTo(new File(fileName));
+            file.transferTo(new File(basePath+fileName));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        //最后要返回文件名称，因为新增菜品中点击保存后需要文件的名称
+        return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+        try {
+            //通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath+name));
+            //通过输出流将文件写回浏览器，在浏览器中展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+            //设置响应类型
+            response.setContentType("img/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
